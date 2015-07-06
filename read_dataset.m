b831eb74cbb1529fe21bb8e091bc89777791c841
rustle1314 <lili1987mail@gmail.com>
2015-07-06 16:18:30 +0000

    read sparse data

--- read_dataset.m
+++ read_dataset.m
@@ -6,7 +6,21 @@ FilePath_Ytt = [DataSet '/Y_tt'];
 FilePath_Xtt = [DataSet '/X_tt'];
 
 y_tr = load(FilePath_Ytr);
-x_tr = load(FilePath_Xtr);
-y_tt = load(FilePath_Ytt);
-x_tt = load(FilePath_Xtt);
+if size(y_tr,2) == 3
+    y_tr = spconvert(y_tr);
+end
 
+x_tr = load(FilePath_Xtr);
+if size(x_tr,2) == 3
+    x_tr = spconvert(x_tr);
+end
+
+y_tt = load(FilePath_Ytt);
+if size(y_tt,2) == 3
+    y_tt = spconvert(y_tt);
+end
+
+x_tt = load(FilePath_Xtt);
+if size(x_tt,2) == 3
+    x_tt = spconvert(x_tt)
+end
